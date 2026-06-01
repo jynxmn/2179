f793227c8ad48913b49fe0a86e5a14857ab7be0d
jynxmn <jyap0048@student.monash.edu>
2026-06-02 05:27:47 +1000

    Added more week 7 studio files

---
+++ week7_studio/vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "daily_cost_bar_chart.vg.json";
+
+//Embed the vegalite visualisation stored in vg_1 in the div container
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    //Access the vega view instance
+}).catch(console.error);
